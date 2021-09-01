ba22b79661437b18ec35cb4b75a6b98111bd4c1f
Vipretha <89765930+Vipretha@users.noreply.github.com>
2021-08-31 21:30:47 -0700

    Add files via upload

---
+++ USERarithmeticoper.py
@@ -0,0 +1,15 @@
+a=int(input("enter a value:"))
+b=int(input("enter a value:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print("arithmetic operators")
+print("value of a",a)
+print("value of b",b)
+print("addition value of a&b",c)
+print("subraction value of a&b",d)
+print("multiplication value of a&b",e)
+print("division value of a&b",f)
+print("modulus value of a&b",g)
---
+++ arithmeticopr.py
@@ -0,0 +1,16 @@
+a=3
+b=3
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print("arithmatic operators")
+print("value of a",a)
+print("value of b",b)
+print("addition value of a&b",c)
+print("subraction value of a&b",d)
+print("multiplication value of a&b",e)
+print("division value of a&b",f)
+print("modulus value of a&b",g)
+
